562b613f843dd50b92c5e950331001661d27d954
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-07-06 22:41:30 +0100

    I have a feeling this is a very messy looking code. But can't quite tell yet, to be honest

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +15,74 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  //ADD ONE
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    const newRecipe = {
+      title: "Beer Chicken",
+      level: "Easy Peasy",
+      ingredients: ["Chicken Drumsticks", "Beer", "Onion Soup Powder"],
+      cuisine: "Comfort Food",
+      dish_type: "main-course",
+      duration: 40,
+      creator: "Chef"
+    };
+
+    return Recipe.create (newRecipe)
+    .then((beerChickenRecipe) => {
+      console.log("Recipe title: ${createdRecipe.title}");
+    })
+    .catch((error) => {
+      console.error("Error creating recipe", error);
+    });
   })
+
+  //ADD MANY
+
+  .then (async () => {
+    try {
+      const recipeList = await Recipe.insertMany(data);
+      recipeList.forEach((recipeFromList) => {
+        console.log("Recipe title: ${recipeFromList.title}");
+      });
+      } catch (error) {
+        console.log("Error creating many recipes", error);
+      }
+    })
+
+    //UPDATE ONE
+
+    .then (async () => {
+      try {
+        const updateRigatoni = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new:true});
+        console.log ("Success Message! Updated Recipe:", updateRigatoni);
+      } catch (error) {
+        console.log("Error updating recipe", error);
+      }
+    })
+
+    //REMOVE ONE
+
+    .then (async () => {
+      try {
+        const deleteCarrotCake = await Recipe.deleteOne({title: "Carrot Cake"});
+        console.log ("Success Message! Deleted Recipe:", deleteCarrotCake);
+      } catch (error) {
+        console.log ("Error deleting recipe", error);
+      }
+    })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  .then (async () => {
+    try {
+      await mongoose.connection.close();
+      console.log("Mongoose connection closed successfully");
+    } catch (error) {
+      console.log ("Connection Close Error:", error);
+    }
+  })
